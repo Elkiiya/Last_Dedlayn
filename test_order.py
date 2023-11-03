a7a164d9e0f05af9bb8a4de0d7faeedb5c10be42
Elkiiya <108469136+Elkiiya@users.noreply.github.com>
2023-11-04 00:14:44 +0300

    Update test_order.py

--- test_order.py
+++ test_order.py
@@ -2,4 +2,9 @@
 import Created_Order
 
 def test_search_order_status_code_200():
-    assert Created_Order.search_order().status_code == 200
+    status_code = Created_Order.search_order().status_code
+    assert status_code == 200
+    if status_code == 200:
+        print("Тест пройден, код 200")
+    else:
+        print("Потрачено")
