6541211d2a95d6e54e065a61f0f15a9699512097
Elkiiya <108469136+Elkiiya@users.noreply.github.com>
2023-11-04 11:41:27 +0300

    Update test_order.py

--- test_order.py
+++ test_order.py
@@ -2,4 +2,4 @@
 import Created_Order
 
 def test_search_order_status_code_200():
-    assert Create_and_Search_Order.search_order().status_code == 200
+    assert Create_Order.search_order().status_code == 200
